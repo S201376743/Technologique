d51d5262442957616c9d17a592c5417329164b54
Alien0531 <alienkie@gmail.com>
2022-08-28 08:39:45 +0200

    login register fuxn busy

--- Technologique_Website/login.aspx.cs
+++ Technologique_Website/login.aspx.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using HashPass;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Technologique_Website.ServiceReference1;
 
 namespace Technologique_Website
 {
     public partial class login : System.Web.UI.Page
     {
+        Service1Client sc = new Service1Client();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            var hashed = Secrecy.HashPassword(txtPassword.Value);
+            int login = sc.userLogin(txtEmail.Value, hashed);
+            if(login != 0)
+            {
+                Session["LoggedInUser"] = login;
+                Response.Redirect("index.aspx");
+            }
         }
     }
 }
